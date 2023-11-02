1a820aefd5ef8c8740d2df17584932b95c0b6ddc
Sujitpandey1409 <sujitpandey1409@gmail.com>
2023-11-02 12:50:55 +0530

    updates

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -15,7 +15,7 @@ console.log('env is :', process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'production') {
     baseURL = process.env.REACT_APP_SERVER_BASE_URL
 }
-console.log(baseURL);
+// console.log(baseURL);
 export const axiosClient = axios.create({
     baseURL,
     withCredentials: true,
