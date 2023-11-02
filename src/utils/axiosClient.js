0566243005681edecadae91c9c82948c6418e735
Sujitpandey1409 <sujitpandey1409@gmail.com>
2023-11-02 12:42:10 +0530

    updates

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -11,7 +11,7 @@ import { TOAST_FAILURE } from "../App";
 
 // let baseURL = 'http://localhost:4000/';
 let baseURL = 'https://sujit-socialmedia2.onrender.com/';
-console.log('env is ', process.env.NODE_ENV);
+console.log('env is :', process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'production') {
     baseURL = process.env.REACT_APP_SERVER_BASE_URL
 }
